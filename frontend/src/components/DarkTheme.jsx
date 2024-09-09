ee14c40758d5eb086bac45ea942f311e69419fab
Dipen <dipendraboyaju222@gmail.com>
2024-09-09 14:58:58 +0545

    change

--- frontend/src/components/DarkTheme.jsx
+++ frontend/src/components/DarkTheme.jsx
@@ -16,7 +16,7 @@ const DarkTheme = ({ theme, onThemeChange }) => {
     localStorage.setItem('theme', newTheme);
   }
   return (
-    <div className={` ${theme === 'light' ? 'bg-[#0C0202] hover:bg-zinc-700 ' : 'bg-zinc-100 hover:bg-zinc-300'} fixed right-0 top-32 z-30 py-3 pl-4 cursor-pointer pr-2 rounded-l-full transition-all duration-500`} onClick={toggleTheme} style={{ color: theme === 'light' ? 'white' : 'black' }}>{theme === "light" ? <Moon color="black" /> : <Sun color="white" />}</div>
+    <div className={` ${theme === 'light' ? 'bg-[#0C0202] hover:bg-zinc-700 ' : 'bg-zinc-100 hover:bg-zinc-300'} fixed right-0 top-32 z-30 py-3 pl-4 cursor-pointer pr-2 rounded-l-full transition-all duration-500`} onClick={toggleTheme} style={{ color: theme === 'light' ? 'white' : 'black' }}>{theme === "light" ? <Moon color="white" /> : <Sun color="black" />}</div>
   )
 }
 export default DarkTheme
