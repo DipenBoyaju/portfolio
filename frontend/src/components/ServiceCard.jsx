05cece44e91af5d35c5e69804d87ac883d1142cf
Dipen <dipendraboyaju222@gmail.com>
2024-09-09 14:48:53 +0545

    change

--- frontend/src/components/ServiceCard.jsx
+++ frontend/src/components/ServiceCard.jsx
@@ -2,7 +2,7 @@ import { MoveUpRight } from "lucide-react"
 
 const ServiceCard = ({ title, icon }) => {
   return (
-    <div className="bg-zinc-200 dark:bg-[rgba(10,0,0,0.7)] rounded-lg p-7 flex flex-col gap-10 cursor-pointer hover:bg-white dark:hover:bg-white  hover:shadow-2xl group">
+    <div className="bg-zinc-200 dark:bg-[rgba(10,0,0,0.7)] rounded-lg p-7 flex flex-col gap-10 cursor-pointer hover:bg-white dark:hover:bg-white  hover:shadow-2xl group w-full">
       <span className="bg-white dark:bg-zinc-900  p-2 rounded-full text-xl h-12 w-12 items-center flex justify-center shadow-lg  group-hover:bg-zinc-100 dark:group-hover:text-zinc-700 dark:text-zinc-400 text-zinc-600">
         {icon}
       </span>
